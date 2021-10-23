0104d2e45ad5fa3967a585d1ef3c4ef42e5e6065
Mario <example@gmail.com>
2021-10-23 19:27:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict.";
+
+/* alert('hello'); */
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = prompt("Do you have 15?", "");
+console.log(answer);
+ */
+
+const answers = [];
+
+answers[0] = prompt("What your name?");
+answers[1] = prompt("Where are you from?");
+answers[2] = prompt("How old are you?");
+
+document.write(answers);
+
+
+console.log(2==4 || 4 == 4);
