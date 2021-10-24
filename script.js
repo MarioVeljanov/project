6d2d2c3554780ff87a71166803047348bbd74a72
Mario <example@gmail.com>
2021-10-24 12:07:53 +0200

    practise two: if/else and for,while,do loops

--- script.js
+++ script.js
@@ -9,13 +9,48 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
- 
-const a = prompt("Еден од последни гледани филмови?");
-const b = prompt("Kolku poeni?"); 
-const c = prompt("Еден од последни гледани филмови?");
-const d = prompt("Kolku poeni?");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 1; i <= 2; i++) {
+    const a = prompt("Еден од последни гледани филмови?"),
+          b = prompt("Колку поени ке му дадеш?");
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        alert("Внесе не соодветни податоци. Обиди се повторно.");
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count <= 10) {
+    alert(`Имаш изгледано ${personalMovieDB.count} филмови- тоа е доволно малку.`);
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    alert(`Имаш изгледано ${personalMovieDB.count} филмови- ти си класичен гледач.`);
+} else if (personalMovieDB.count > 30) {
+    alert(`Имаш изгледано ${personalMovieDB.count} филмови- ти си киноман.`);
+} else {
+    alert("Има некаква грешка при внесување на податоци за количина на изгледани филмови");
+}
 
 console.log(personalMovieDB);
+
+
+/* let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++; 
+}; 
+
+ do {
+    console.log(num);
+    num++;
+}
+while (num <= 55); 
+
+for (let i =1; i < 8; i++) {
+    console.log(i);
+    num++;
+}
+console.log(num);
+ */
